97639e73a96ae275165938c46309336ea77f697d
duync <duync@coccoc.com>
2024-09-04 03:22:53 +0700

    Update MetaEnv format

--- src/main/koh/db/hub/metadata/MetaEnv.java
+++ src/main/koh/db/hub/metadata/MetaEnv.java
@@ -2,9 +2,22 @@ package koh.db.hub.metadata;
 
 import lombok.Data;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class MetaEnv {
-    private final Map<String, String> envMap;
+    Boolean activation = false;
+    Integer volumeLimit = 4 * 1024;
+    Integer containerLimit = 1;
+    Integer memoryLimit = 512;
+    Integer databaseLimit = 1;
+    List<Credential> credentials = new ArrayList<>();
+
+    @Data
+    public static class Credential {
+        Long rid = null;
+        String type = null;
+        String content = null;
+    }
 }
--- src/main/koh/db/hub/repository/UserMetadataRepository.java
+++ src/main/koh/db/hub/repository/UserMetadataRepository.java
@@ -7,26 +7,23 @@ import org.jooq.Record;
 import org.jooq.Table;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserMetadataRepository extends AbstractRepository {
-    public List<UserMetadataRecord> getMetadataByUserId(Long userId) {
+    public List<UserMetadataRecord> getMetadata(Long userId) {
         return DataHub.useContext().fetch(USER_METADATA.where(USER_METADATA.USER_ID.eq(userId)));
     }
 
-    public List<UserMetadataRecord> getMetadataByUserEmail(String email) {
+    public Optional<UserMetadataRecord> getMetadata(String email, UserMetadataType type) {
         Table<Record> table = USER_METADATA
                 .join(USER)
                 .on(USER.ID.eq(USER_METADATA.USER_ID))
-                .where(USER.EMAIL.eq(email));
-        return DataHub.useContext().fetch(table).into(UserMetadataRecord.class);
+                .where(USER.EMAIL.eq(email).and(USER_METADATA.TYPE.eq(type)));
+        return Optional.ofNullable(DataHub.useContext().fetchOne(table)).map(r -> r.into(UserMetadataRecord.class));
     }
 
-    public UserMetadataRecord createMetadata(
-            Long userId,
-            String name,
-            UserMetadataType type,
-            byte[] data,
-            boolean hidden
+    public Optional<UserMetadataRecord> createMetadata(
+            Long userId, String name, UserMetadataType type, byte[] data, boolean hidden
     ) {
         UserMetadataRecord r = DataHub
                 .useContext()
@@ -36,10 +33,10 @@ public class UserMetadataRepository extends AbstractRepository {
                 .setType(type)
                 .setBlob(data)
                 .setHidden(hidden);
-        return r.insert() == 1 ? r : null;
+        return r.insert() == 1 ? Optional.of(r) : Optional.empty();
     }
 
-    public UserMetadataRecord createSecretMetadata(Long userId, String name, byte[] data) {
+    public Optional<UserMetadataRecord> createSecretMetadata(Long userId, String name, byte[] data) {
         UserMetadataRecord r = DataHub
                 .useContext()
                 .newRecord(USER_METADATA)
@@ -48,15 +45,15 @@ public class UserMetadataRepository extends AbstractRepository {
                 .setType(UserMetadataType.ENVIRONMENT)
                 .setBlob(data)
                 .setHidden(true);
-        return r.insert() == 1 ? r : null;
+        return r.insert() == 1 ? Optional.of(r) : Optional.empty();
     }
 
-    public UserMetadataRecord updateMetadata(Long id, byte[] data) {
+    public Optional<UserMetadataRecord> updateMetadata(Long id, byte[] data) {
         UserMetadataRecord r = DataHub.useContext().fetchOne(USER_METADATA.where(USER_METADATA.ID.eq(id)));
         if (r != null) {
             r.setBlob(data);
-            return r.insert() == 1 ? r : null;
+            return r.insert() == 1 ? Optional.of(r) : Optional.empty();
         }
-        return null;
+        return Optional.empty();
     }
 }
